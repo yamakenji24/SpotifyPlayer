ac029a90e8d2b0f751172715ea021ffb407e102e
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-08-02 21:48:24 +0900

    authorization code flows

--- src/authentication.js
+++ src/authentication.js
@@ -1,26 +1,44 @@
 import {
-  clientId, clientSecret, redirectUri
+  clientId, clientSecret, redirectUri, b64Auth
 } from './constants/server-constants';
 
-function authentication() {
+const getAuthCode = () => {
   if(/localhost:3000\/$/.test(window.location.href)) {    
     window.location.replace(
-      "https://accounts.spotify.com/authorize?client_id="+clientId+"&redirect_uri="+redirectUri+"&response_type=token&state=123"
+      "https://accounts.spotify.com/authorize?client_id="+clientId+"&redirect_uri="+redirectUri+"&response_type=code&state=123"
     )
   }
-  let url = window.location.href
-  let accessToken = url.match(/#(?:access_token)=([\S\s]*?)&/)[1]
+  const url = window.location.href
+  return url.match(/(?:code)=([\S\s]*?)&/)[1]
+}
+
+const getToken = async (authCode) => {
+  const response =  await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${b64Auth}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `grant_type=authorization_code&code=${authCode}&redirect_uri=${redirectUri}`,
+  })
+  
+  const {
+    access_token: accessToken,
+    refresh_token: refreshToken,
+    expires_in: expiresIn,
+  } = await response.json()
+  sessionStorage.setItem('accessToken', accessToken);
+  sessionStorage.setItem('refreshToken', refreshToken)
   return accessToken;
 }
 
-export default function getAuthToken() {
+const getAuthToken =  async () => {
   //sessionStorage.clear()
   let accessToken = sessionStorage.getItem('accessToken');
-  if (accessToken) {
-    return accessToken;
-  } else {
-    accessToken = authentication();
-    sessionStorage.setItem('accessToken', accessToken);
-    return accessToken;
+  if (!accessToken || accessToken === undefined) {
+    const authCode = await getAuthCode();
+    accessToken = await getToken(authCode)
   }
+  return accessToken
 }
+export default getAuthToken;
