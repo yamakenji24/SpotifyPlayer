b9d544fad618949c160dbf2ce3d3acc383197f6d
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-08-02 15:49:18 +0900

    整頓

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import getAuthToken from './authentication';
-import configureStore from './store';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom'
-
-const store = configureStore()
 
 //将来的には、react-routerを利用してclient-idを持ってない人用のアクセスを用意する
 const accessToken = getAuthToken()
 
 ReactDom.render(
-  <Provider store = {store}>
-    <BrowserRouter>
-      <div>
-        <Route exact path='/'>
-          <Redirect to={'/app'} />
-        </Route>
-        <Route exact path='/app'>
-          <App token={accessToken}/>
-        </Route>
-      </div>
-    </BrowserRouter>
-  </Provider>,
+  <div>
+    Hello
+  </div>,
   document.getElementById('root')
 );
 
