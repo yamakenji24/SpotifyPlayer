32ebfe749c63002faf2eeafb97d77d8f9b9b8951
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-08-08 00:33:16 +0900

    get token

--- src/components/TopPage/index.tsx
+++ src/components/TopPage/index.tsx
@@ -1,16 +1,49 @@
-import React, {FC} from 'react';
+import React, {FC, useState, useEffect} from 'react';
 import styled from 'styled-components';
+import getAuthToken from '../../authentication';
+import {useHistory} from 'react-router-dom';
 
 const TopPage: FC = () => {
+  const [hasToken, sethasToken] = useState(false)
+  const history = useHistory();
+
+  useEffect(() => {
+    getAuthToken()
+    history.push('/search')
+  }, [])
+  /*
+  useEffect (() => {
+    if (hasToken) {
+      getAuthToken()
+    }
+  }, [hasToken])*/
+
+  const handleClick = () => {
+    sethasToken(true)
+  }
+
   return (
-    <Title>
-      Welcome to my TopPage
-    </Title>
+    <>
+      <Title>
+        Welcome to my TopPage
+      </Title>
+
+      <Button onClick={handleClick}>
+        Login 
+      </Button>
+    </>
   )
 }
 
 const Title = styled.h1`
   text-align: center;
+`;
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
 `;
 
 export default TopPage;
