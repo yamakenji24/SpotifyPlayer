909104c8e537af8daa1fae9a77381218b1a4aa33
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-08-12 18:41:20 +0900

    define types for tracks data

---
+++ src/components/SearchMusic/Tracks/trackType.ts
@@ -0,0 +1,40 @@
+import {albumsItems, artists} from '../Albums/albumType';
+
+export interface trackType {
+  id: string,
+  name: string,
+  artists: string,
+  playUrl: string | null,
+}
+
+export interface tracksJsonType {
+  tracks: {
+    href: string,
+    items: trackItems[],
+    limit: number,
+    next: string,
+    offset: number,
+    previous: string | null,
+    total: number
+  }
+}
+
+interface trackItems {
+  album: albumsItems,
+  artists: artists[],
+  avaiable_markets: string[],
+  disc_number: number,
+  duration_ms: number,
+  explicit: boolean,
+  external_ids: {[key: string]: string},
+  external_urls: {[key: string]: string}
+  href: string,
+  id: string,
+  is_local: boolean,
+  name: string,
+  popularity: number,
+  preview_url: string | null,
+  track_number: number,
+  type: string,
+  uri: string
+}
