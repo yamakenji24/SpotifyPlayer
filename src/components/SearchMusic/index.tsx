5a76e7e7110cb036f5985bd117e7924ef0c368bd
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-08-12 18:36:55 +0900

    change display by panel tab

--- src/components/SearchMusic/index.tsx
+++ src/components/SearchMusic/index.tsx
@@ -1,12 +1,36 @@
-import React, {FC, useState, useEffect} from 'react';
+import React, {FC, useState} from 'react';
 import Artists from './Artists';
+import Albums from './Albums';
+import Tracks from './Tracks';
 import {useSearch} from './useSearch';
 
+const ARTIST_PANEL = 'ARTIST_PANEL';
+const ALBUM_PANEL = 'ALBUM_PANEL';
+const TRACK_PANEL = 'TRACK_PANEL';
+
 const SearchMusic: FC = () => {
-  const [fetchData, {artists, albums}] = useSearch()
+  const [fetchData, {artists, albums, tracks}] = useSearch()
+  const [panel, setPanel] = useState(ARTIST_PANEL)
 
   const handleonChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     fetchData(e.target.value)
+  }
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    setPanel(e.currentTarget.name)
+  }
+
+  const switchPanel = () => {
+    switch(panel) {
+      case ARTIST_PANEL:
+        return <Artists artists={artists}/>
+      case ALBUM_PANEL:
+        return <Albums albums={albums} />
+      case TRACK_PANEL:
+        return <Tracks tracks={tracks} />
+      default:
+        return null
+    }
   }
 
   return (
@@ -18,7 +42,15 @@ const SearchMusic: FC = () => {
         onChange={handleonChange}
       />
 
-      <Artists artists={artists} />
+      <div className="panel_button">
+        <button name={ARTIST_PANEL} onClick={handleOnClick}>Artists</button>
+        <button name={ALBUM_PANEL} onClick={handleOnClick}>Albums</button>
+        <button name={TRACK_PANEL} onClick={handleOnClick}>Tracks</button> 
+      </div>    
+
+      <div className="panel_display">
+        {switchPanel()}
+      </div>
     </div>
   )
 }
