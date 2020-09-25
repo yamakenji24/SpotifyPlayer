1a9124ed372ca84ad83f74f658db11da32c946c2
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-09-25 18:57:40 +0900

    play mp3 audio

--- src/ui/music/components/Tracks.tsx
+++ src/ui/music/components/Tracks.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
 import { trackType } from '../../../types/trackType';
+import { useAudio } from '../hooks/useAudio';
 import './track.css';
 
 interface Props {
   tracks: trackType[];
 }
+
+const AudioPlayer = (playUrl: string) => {
+  const [playing, audio] = useAudio(playUrl);
+
+  const handleOnClick = () => {
+    playing ? audio.pause() : audio.play()
+  }
+
+  return (
+    <>
+      <button onClick={handleOnClick}>
+        {playing ? "Pause" : "Play"}
+      </button>
+    </>
+  );
+};
 
 export const Tracks = ({tracks}: Props): JSX.Element => {
   return (
@@ -14,9 +31,11 @@ export const Tracks = ({tracks}: Props): JSX.Element => {
           <div key={track.id}>
             <p>{idx + 1}</p>
             <h4>{track.name}</h4>
+            {track.playUrl ? AudioPlayer(track.playUrl): <p>no MP3 URL</p>}
           </div>  
         )
-        : <><p>Now Loading Tracks...</p></>
+        : <><p>Now Loading Tracks...</p>
+        </>
       }
     </>
   )
