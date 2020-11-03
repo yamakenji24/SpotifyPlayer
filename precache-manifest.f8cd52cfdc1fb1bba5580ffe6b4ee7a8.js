be0b71e73e37b6a9cb2ed463f6ffcb43fc129885
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-11-03 14:56:05 +0900

    Updates

---
+++ precache-manifest.f8cd52cfdc1fb1bba5580ffe6b4ee7a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37437f8e15f5a2b3b06dabf8c0aad11d",
+    "url": "/SpotifyPlayer/index.html"
+  },
+  {
+    "revision": "1894c3775f6ff1eecf42",
+    "url": "/SpotifyPlayer/static/css/main.f8cf6019.chunk.css"
+  },
+  {
+    "revision": "4aa63ac128f6994e6a4d",
+    "url": "/SpotifyPlayer/static/js/2.022a2be8.chunk.js"
+  },
+  {
+    "revision": "6a1f60932d4b758cc1944d93356f455d",
+    "url": "/SpotifyPlayer/static/js/2.022a2be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1894c3775f6ff1eecf42",
+    "url": "/SpotifyPlayer/static/js/main.211a2fbd.chunk.js"
+  },
+  {
+    "revision": "2bdb81e37b9cba806b0a",
+    "url": "/SpotifyPlayer/static/js/runtime-main.93b8ac74.js"
+  }
+]);
